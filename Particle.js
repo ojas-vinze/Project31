5bd48fcd84236df9a8c7e0a3ec927239a7dab080
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2020-11-28 10:47:16 +0530

    Update Particle.js

--- Particle.js
+++ Particle.js
@@ -1,20 +1,21 @@
 class Particle{
     constructor(x,y,r){
         var options = {
-            isStatic:false
+            isStatic:false,
+            restitution:0.4
         }
         this.x=x
         this.y=y
         this.r=r
         this.body=Bodies.circle(this.x,this.y,this.r,options)
-        this.color=(color(random(0,255)),color(random(0,255)),color(random(0,255)))
+        this.color=color(random(0,255),random(0,255),random(0,255));
         World.add(world,this.body)
     }
 
     display(){
         var ppos = this.body.position;
-        ellipseMode(CENTER);
+        ellipseMode(RADIUS);
         fill(this.color);
-        ellipse(ppos.x,ppos.y,this.r)
+        ellipse(ppos.x,ppos.y,this.r,this.r);
     }
 }
